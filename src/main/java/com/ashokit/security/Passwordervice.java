c8ccca5d46c28ca54491d523251eef559aaba973
Arjun Patil <arbpatil07@gmail.com>
2023-02-13 21:51:03 +0530

    j var

--- src/main/java/com/ashokit/security/Passwordervice.java
+++ src/main/java/com/ashokit/security/Passwordervice.java
@@ -5,7 +5,9 @@ import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 
 public class Passwordervice {
+	
  int i=10;
+ int j=20;
 	public static String encode(String txt) {
 
 		Encoder encoder = Base64.getEncoder();
